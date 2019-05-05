82c94fde537bcc72df46b3859007ef4311c92e01
Janek <jan.g.grzybek@gmail.com>
2019-05-05 11:40:18 +0200

    Execution functions excluded

--- server/communication/jobs.c
+++ server/communication/jobs.c
@@ -80,3 +80,57 @@ int execute_keys(hash_table *hash, int client_fd)
 
     return 1;
 }
+
+int do_job(hash_table *hash, mcache_request request, int client_fd)
+{
+    switch (request.header.command)
+    {
+    case INSERT:
+        debug_print("INSERT", 1);
+        execute_insert(hash, request, client_fd);
+        debug_print("INSERT", 0);
+        break;
+    case GET:
+        debug_print("GET", 1);
+        execute_get(hash, request, client_fd);
+        debug_print("GET", 0);
+        break;
+    case POP:
+        debug_print("POP", 1);
+        execute_pop(hash, request, client_fd);
+        debug_print("POP", 0);
+        break;
+    case KEYS:
+        debug_print("KEYS", 1);
+        execute_keys(hash, client_fd);
+        debug_print("KEYS", 0);
+        break;
+    case ALL:
+        debug_print("ALL", 1);
+        execute_get(hash, request, client_fd);
+        debug_print("ALL", 0);
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
+
+int read_data_send_response(hash_table *hash, int client_fd)
+{
+    debug_print("read_data_send_response", 1);
+    mcache_request request = read_request(client_fd);
+    if (request.code != 0)
+    {
+        debug_print("Error parsing request", 2);
+        debug_print_int((int)request.header.command);
+        debug_print("read_data_send_response", 0);
+        return 0;
+    }
+    debug_print_int((int)request.header.command);
+    int result = do_job(hash, request, client_fd);
+
+    debug_print("read_data_send_response", 0);
+
+    return !result;
+}
--- server/communication/jobs.h
+++ server/communication/jobs.h
@@ -6,3 +6,5 @@ int execute_get(hash_table *, mcache_request, int);
 int execute_pop(hash_table *, mcache_request, int);
 int execute_keys(hash_table *, int);
 int execute_all(hash_table *, mcache_request, int);
+int do_job(hash_table *, mcache_request, int);
+int read_data_send_response(hash_table *, int);
