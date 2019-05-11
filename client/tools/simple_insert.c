6df0c3b2bef84f7811e59071266bba3e63974f79
Janek <jan.g.grzybek@gmail.com>
2019-05-11 15:36:51 +0200

    avg time

--- client/tools/simple_insert.c
+++ client/tools/simple_insert.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 
 #include "../lib/mcache_client.h"
 
 #define BUFFER_SIZE 1024
+
+long getMicrotime()
+{
+    struct timeval currentTime;
+    gettimeofday(&currentTime, NULL);
+    return currentTime.tv_sec * (int)1e6 + currentTime.tv_usec;
+}
 
 void print_exit_message(int c)
 {
@@ -28,14 +36,19 @@ int main(int argc, char **argv)
          value_buffer[BUFFER_SIZE] = {0};
 
     int i = 0;
+    long long int total_time = 0;
     for (; scanf("%s", key_buffer) == 1 && scanf("%s", value_buffer) == 1; ++i)
     {
+        long long int start_time = getMicrotime();
         query_result qr = mcache_insert_strings(&params, key_buffer, value_buffer);
         if (qr.code != 0)
             break;
+        long long int stop_time = getMicrotime();
+        total_time += stop_time - start_time;
         memset(key_buffer, 0, BUFFER_SIZE);
         memset(value_buffer, 0, BUFFER_SIZE);
     }
     print_exit_message(i);
+    printf("avg time for insert = %fs", (float)(total_time / i + 1) / (1000 * 1000));
     return 0;
 }
