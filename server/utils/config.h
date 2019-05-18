91b9fe519ca5bcb0548be186c06b4314f4c87037
Janek <jan.g.grzybek@gmail.com>
2019-05-18 19:07:51 +0200

    merging

--- server/utils/config.h
+++ server/utils/config.h
@@ -6,14 +6,9 @@ typedef struct
         error_log,
         server_port;
     char *save_path,
-<<<<<<< HEAD
-        *server_address;
-=======
         *server_address,
         *traffic_file,
         *error_file;
-    int server_port;
->>>>>>> logger
 } config_values;
 
 config_values *read_config(char *);
